708e0524c60dd860df2b314f7c39e35e43a800c6
Aarav Parikh <aaravp1223@gmail.com>
2025-07-09 13:50:50 -0400

    chore(doc): Add XSS

---
+++ dataset/cwe_79_xss/xss_1/app.py
@@ -0,0 +1,18 @@
+from markupsafe import escape
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/xss")
+def vulnerable_xss():
+    name = request.args.get("name", "")
+    return f"<h1>Hi {name}!</h1>"
+
+@app.route("/xss_sanitized")
+def sanitized_xss():
+    name = request.args.get("name", "")
+    name = escape(name)
+    return f"<h1>Hi {name}!</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
