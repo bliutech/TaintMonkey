2d49661510128acebf7930e4c478cb469cf8852f
Aarav Parikh <aaravp1223@gmail.com>
2025-07-09 16:13:41 -0400

    feat(add): added trailing newline and function

--- dataset/cwe_79_xss/xss_1/app.py
+++ dataset/cwe_79_xss/xss_1/app.py
@@ -3,16 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+def sayHi(name):
+    return f"<h1>Hi {name}!</h1>"
+
 @app.route("/xss")
 def vulnerable_xss():
     name = request.args.get("name", "")
-    return f"<h1>Hi {name}!</h1>"
+    return sayHi(name)
 
 @app.route("/xss_sanitized")
 def sanitized_xss():
     name = request.args.get("name", "")
     name = escape(name)
-    return f"<h1>Hi {name}!</h1>"
+    return sayHi(name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
