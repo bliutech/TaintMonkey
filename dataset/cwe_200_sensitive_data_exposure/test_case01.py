3e8c4adce3176da860a1dd8358869006ba80c5bf
Carter K Chew <163763403+Carter-Chew@users.noreply.github.com>
2025-07-09 16:03:56 +0000

    Edit test_case01 and test_case02
    incorporated suggested changes from Bliu

--- dataset/cwe_200_sensitive_data_exposure/test_case01.py
+++ dataset/cwe_200_sensitive_data_exposure/test_case01.py
@@ -6,23 +6,24 @@ app = Flask(__name__)
 @app.post("/insecure_register")
 def register_insecure_ssnum ():
     ssnum = request.args.get("ssnum")
-    if ssnum is None or "":
-        return ("Please enter a valid SSN", 400)
-    else:
-        app.logger.info(f"SSN entered: {ssnum}")
-        return ("SS Number: " + ssnum, 200)
+    if not ssnum:
+        return "Please enter a valid SSN", 400
+    app.logger.info(f"SSN entered: {ssnum}")
+    return "SS Number: " + ssnum, 200
 
 @app.post("/secure_register")
 def register_secure_ssnum():
     ssnum = request.args.get("ssnum")
-    if ssnum is None or "":
-        return ("Please enter a valid SSN", 400)
-    else:
-        anon = hashlib.sha256(ssnum.encode()).hexdigest()
-        app.logger.info(f"SSN entered: {anon}")
-        return ("SS Number: " + anon, 200)
+    if not ssnum:
+        return "Please enter a valid SSN", 400
+    anon = hash_ssn(ssnum)
+    app.logger.info(f"SSN entered: {anon}")
+    return "SS Number: " + anon, 200
 
+def hash_ssn(ssnum):
+    hashed= hashlib.sha256(ssnum.encode()).hexdigest()
+    return hashed
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
 
