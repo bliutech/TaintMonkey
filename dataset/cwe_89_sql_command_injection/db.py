1b7664e784d738657324fb5645a398e760059a9d
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-07-09 13:09:41 -0400

    fix(data): changed request.form to request.args.get, added requirements.txt trainline

--- dataset/cwe_89_sql_command_injection/db.py
+++ dataset/cwe_89_sql_command_injection/db.py
@@ -1,14 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
-# Create SQLAlchemy instance without binding to an app yet
+
 db = SQLAlchemy()
 
+
 def init_db(app):
-    """Initialize the database with the given Flask app"""
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    
-    # Create tables
-    with app.app_context():
-        db.create_all()
+   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   db.init_app(app)
+  
+   with app.app_context():
+       db.create_all()
