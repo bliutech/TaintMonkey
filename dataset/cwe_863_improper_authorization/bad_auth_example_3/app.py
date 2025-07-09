07a6b937ecd5e7c4b6552e84ff775bbd792ecc24
SeabassMarket <simercado07@gmail.com>
2025-07-09 01:35:26 -0400

    feat(data): finish cve 863 improper authorization trio

--- dataset/cwe_863_improper_authorization/bad_auth_example_3/app.py
+++ dataset/cwe_863_improper_authorization/bad_auth_example_3/app.py
@@ -1,25 +1,29 @@
-from flask import Flask, request, session, redirect
+from flask import Flask, request, session
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
-# Fake user database where key is username & value is password
+# Fake user database
 users = {
-    "alice": "alice123"
+    "alice": {"password": "alice123", "token": "alice secret text"},
+    "jeff": {"password": "jeff123", "token": "jeff secret text"},
 }
 
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+@app.post("/login")
+def login_send():
+    username = request.form["username"]
+    password = request.form["password"]
 
-        if username in users and users[username] == password:
-            session["username"] = username
-            return f"Welcome, {username}!"
-        return "Invalid credentials"
+    user = users.get(username)
+    if user and user["password"] == password:
+        session["username"] = username
+        return f"Welcome, {username}!"
+    return "Invalid credentials"
 
+
+@app.get("/login")
+def login_show():
     return '''
         <form method="post">
             <h2>Login</h2>
@@ -28,31 +32,56 @@ def login():
             <input type="submit" value="Login">
         </form>
         <br>
-        <form action="/logout" method="get">
+        <form action="/logout" method="post">
             <button type="submit">Logout</button>
         </form>
-        <form action="/signup" method="get">
-            <button type="submit">Sign Up</button>
+        <form action="/secure/signup" method="get">
+            <button type="submit">Secure Sign Up</button>
+        </form>
+        <form action="/insecure/signup" method="get">
+            <button type="submit">Insecure Sign Up</button>
+        </form>
+        <form action="/secret" method="post">
+            <button type="submit">Open Secret Token</button>
         </form>
     '''
 
 
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        username = request.form["username"]
-        password = request.form["password"]
+def is_user_in_session(user_string, this_session):
+    return user_string in this_session
 
-        if username in users:
-            return "username already taken"
-        users[username] = password
-        return f"{username}, registered!"
 
+@app.post("/secret")
+def secret():
+    if not is_user_in_session("username", session):
+        return f"Session not logged in"
+    username = session["username"]
+
+    #This should never be true
+    if not user_taken(username, users):
+        return "this should not happen"
+    token = users[username]["token"]
+    return f"{username}'s secret token is {token}!"
+
+
+@app.post("/insecure/signup")
+def insecure_signup_send():
+    username = request.form["username"]
+    password = request.form["password"]
+    token = request.form["token"]
+
+    #Doesn't include userTaken function, meaning someone can override another person's account
+    users[username] = {"password": password, "token": token}
+    return f"{username}, registered!"
+
+@app.get("/insecure/signup")
+def insecure_signup_get():
     return '''
-        <form action="/signup" method="post">
+        <form action="/insecure/signup" method="post">
             <h2>Sign Up</h2>
             Username: <input name="username"><br>
             Password: <input name="password" type="password"><br>
+            Secret Token: <input name="token" type="password"><br>
             <input type="submit" value="Sign Up">
         </form>
         <br>
@@ -62,43 +91,41 @@ def signup():
     '''
 
 
-def is_user_in_session(user_string, this_session):
-    return user_string in this_session
+#Monkey Patch function
+def user_taken(user, database):
+    return user in database
 
 
-@app.route("/insecure/delete_self")
-def insecure_delete_user():
-    if not is_user_in_session("username", session):
-        return redirect("/login")
+@app.post("/secure/signup")
+def secure_signup_send():
+    username = request.form["username"]
+    password = request.form["password"]
+    token = request.form["token"]
 
-    user_to_delete = request.args.get("user")
-    if not user_to_delete:
-        return "No URL parameter"
-
-    #CWE-863: No check if current user is itself
-    return f"User {user_to_delete} deleted (pretend)"
-
-
-@app.route("/secure/delete_self")
-def secure_delete_user():
-    if not is_user_in_session("username", session):
-        return redirect("/login")
-
-    user_to_delete = request.args.get("user")
-    if not user_to_delete:
-        return "No URL parameter"
-
-    # CWE-863: No check if current user is itself
-    if user_to_delete != session["username"]:
-        return "Not allowed to delete someone other than self"
-    session.clear()
-    if user_to_delete in users:
-        users.pop(user_to_delete)
-        return "this should not happen"
-    return f"User {user_to_delete} deleted"
+    if user_taken(username, users):
+        return "username already taken"
+    users[username] = {"password": password, "token": token}
+    return f"{username}, registered!"
 
 
-@app.route("/logout")
+@app.get("/secure/signup")
+def secure_signup_get():
+    return '''
+        <form action="/secure/signup" method="post">
+            <h2>Sign Up</h2>
+            Username: <input name="username"><br>
+            Password: <input name="password" type="password"><br>
+            Secret Token: <input name="token" type="password"><br>
+            <input type="submit" value="Sign Up">
+        </form>
+        <br>
+        <form action="/login" method="get">
+            <button type="submit">Login</button>
+        </form>
+    '''
+
+
+@app.post("/logout")
 def logout():
     if not session:
         return "No Session"
