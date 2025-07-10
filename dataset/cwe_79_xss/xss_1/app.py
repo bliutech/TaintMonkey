a4a722c002912b7a383947a75505e8240fab3a6a
Aarav Parikh <aaravp1223@gmail.com>
2025-07-10 07:54:38 -0400

    feat(add): added snake case to xss_1

--- dataset/cwe_79_xss/xss_1/app.py
+++ dataset/cwe_79_xss/xss_1/app.py
@@ -3,19 +3,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-def sayHi(name):
+def say_hi(name):
     return f"<h1>Hi {name}!</h1>"
 
 @app.route("/xss")
 def vulnerable_xss():
     name = request.args.get("name", "")
-    return sayHi(name)
+    return say_hi(name)
 
 @app.route("/xss_sanitized")
 def sanitized_xss():
     name = request.args.get("name", "")
     name = escape(name)
-    return sayHi(name)
+    return say_hi(name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
