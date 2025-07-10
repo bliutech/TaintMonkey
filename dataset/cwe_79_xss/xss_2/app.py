b07d0d1339f16985a01ac7c79e91e90f6157865c
Aarav Parikh <aaravp1223@gmail.com>
2025-07-10 14:15:47 -0400

    feat(add): edit features

--- dataset/cwe_79_xss/xss_2/app.py
+++ dataset/cwe_79_xss/xss_2/app.py
@@ -1,22 +1,24 @@
+#https://www.freeformatter.com/html-escape.html
+
 from flask import Flask, request
 import html
 
 app = Flask(__name__)
 
-def sayHi(name):
+def say_hi(name):
     return f"<h1>Hi {name}!</h1>"
 
 @app.route("/xss")
 def vulnerable_xss():
-    name = request.args.get("name", "")
-    return sayHi(name)
+    name = request.args.get("name", "user")
+    return say_hi(name)
 
 @app.route("/xss_sanitized")
 def sanitized_xss():
-    name = request.args.get("name", "")
-    # html
+    name = request.args.get("name", "user")
+    # html escape
     name = html.escape(name)
-    return sayHi(name)
+    return say_hi(name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
