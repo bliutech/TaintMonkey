e07c940f2b2eab00a74148015f6eaa5b3f885b74
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-07-10 00:13:55 -0400

    feat(data): secure-signup with alpha-numeric

--- dataset/cwe_89_sql_injection/cwe_89_testcase2/app.py
+++ dataset/cwe_89_sql_injection/cwe_89_testcase2/app.py
@@ -55,5 +55,30 @@ def secure_login():
         return "Login successful"
     return "Invalid login", 401
 
+@app.route('/secure-signup', methods=['POST'])
+def iecure_signup():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    
+    # quick return if not valid response
+    if not username or not password:
+        return "Username and password are required", 400
+    
+    error = pattern_match(username, password)
+    if error:
+        return error
+    
+    # takes the command as a string, no affect on system
+    query = text(f"INSERT INTO user (username, password) VALUES (:username, :password)")
+    
+    try:
+        # runs username and password direct because already checked
+        db.db.session.execute(query, {"username": username, "password": password})
+        db.db.session.commit()
+        return "User created securely", 201
+    except Exception as e:
+        db.db.session.rollback()
+        return f"Error creating user", 500
+    
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
