548a0523f5f92fae4865c9d3402a315eff40d6b5
Aarav Parikh <aaravp1223@gmail.com>
2025-07-10 15:51:51 -0400

    feat(add): redid xss_3

--- dataset/cwe_79_xss/xss_3/app.py
+++ dataset/cwe_79_xss/xss_3/app.py
@@ -1,62 +1,22 @@
+#https://bleach.readthedocs.io/en/latest/clean.html
 from flask import Flask, request
 import bleach
 
 app = Flask(__name__)
 
-@app.route("/insecure_signup", methods=["GET", "POST"])
-def unsafe_signup():
-    if request.method == "POST":
-        username = request.form.get("username", "")
-        return f"<h2>Welcome {username}!</h2>"
-    return '''
-        <h3>Vulnerable Signup</h3>
-        <form method="POST">
-        Username: <input name="username"><br>
-        <input type="submit" value="Sign Up">
-    </form>
-    '''
+def welcome(name):
+    return f"<h1>Welcome to GSET, {name}!</h1>"
 
-@app.route("/insecure_login", methods=["GET", "POST"])
-def unsafe_login():
-    if request.method == "POST":
-        username = request.form.get("username", "")
-        return f"<h2>Welcome back {username}!</h2>"
-    return '''
-        <h3>Vulnerable Login</h3>
-        <form method="POST">
-            Username: <input name="username"><br>
-            <input type="submit" value="Login">
-        </form>
-    '''
+@app.get("/insecure")
+def insecure_xss():
+    name = request.args.get("name", "user")
+    return welcome(name)
 
-@app.route("/secure_signup", methods=["GET", "POST"])
-def secure_signup():
-   if request.method == "POST":
-    username = request.form.get("username", "")
-    # bleach (signup)
-    cleanedUsername = bleach.clean(username, tags=[], attributes={}, strip=True)
-    return f"<h2>Welcome {cleanedUsername}!</h2>"
-   return '''
-    <h3>Secure Signup</h3>
-    <form method="POST">
-        Username: <input name="username"><br>
-        <input type="submit" value="Sign Up">
-    </form>
-   '''
-
-@app.route("/secure_login", methods=["GET", "POST"])
-def secure_login():
-    if request.method == "POST":
-        username = request.form.get("username", "")
-        # bleach (login)
-        cleanedUsername = bleach.clean(username, tags=[], attributes={}, strip=True)
-        return f"<h2>Welcome back {cleanedUsername}!</h2>"
-    return '''
-        <h3>Secure Login</h3>
-        <form method="POST">
-            Username: <input name="username"><br>
-            <input type="submit" value="Login">
-    '''
+@app.get("/secure")
+def secure_xss():
+    name = request.args.get("name", "user")
+    name = bleach.clean(name)
+    return welcome(name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
