248b071aaceb04e095e2edf90fa405bd6c181378
Aarav Parikh <aaravp1223@gmail.com>
2025-07-11 01:35:17 -0400

    feat(add): added trailing line and fixed bug

--- dataset/cwe_79_xss/xss_8/app.py
+++ dataset/cwe_79_xss/xss_8/app.py
@@ -9,16 +9,19 @@ def set_cookie():
     resp.set_cookie("username", "Aarav")
     return resp
 
+# <script>alert(document.cookie)</script>
+# xss attack ^
 @app.route("/insecure_cookie")
 def insecure_cookie():
-    username = request.args.get("username")
+    username = request.args.get("username", "")
     return f"How are you, {username}?"
 
 @app.route("/secure_cookie")
 def secure_cookie():
-    username = request.args.get("username")
+    username = request.args.get("username", "")
     username = escape(username)
     return f"How are you, {username}?"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)  
+
