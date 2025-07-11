d870307045e21a78f9a140cddb83dbf50283dc9a
Aarav Parikh <aaravp1223@gmail.com>
2025-07-11 02:14:46 -0400

    feat(add): xss_9 and requirements.txt

---
+++ dataset/cwe_79_xss/xss_9/app.py
@@ -0,0 +1,37 @@
+## https://markupsafe.palletsprojects.com/en/stable/
+from markupsafe import escape
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def table(name, age):
+    return f"""
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+        </tr>
+        <tr>
+            <td>{name}</td>
+            <td>{age}</td>
+        </tr>
+    </table>
+    """
+
+@app.route("/insecure_table")
+def insecure_table():
+    name = request.args.get("name", "")
+    age = request.args.get("age", "")
+    return table(name, age)
+
+@app.route("/secure_table")
+def secure_table():
+    name = request.args.get("name", "")
+    name = escape(name)
+    age = request.args.get("age", "")
+    age = escape(age)
+    return table(name, age)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
