970485729314a87ce3cc4eb1b511c744d71c482d
unknown <carterkylechew@gmail.com>
2025-07-14 15:11:55 -0400

    Formatted test cases using ruff

--- dataset/cwe_938_unvalidated_redirects_and_forwards/requests_prevent_automatic_redirect/test_case04.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/requests_prevent_automatic_redirect/test_case04.py
@@ -4,6 +4,7 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = request.args.get("url")
@@ -11,24 +12,28 @@ def unvalidated_redirect():
         return "No URL provided", 400
     return redirect(redirect_url)
 
+
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = request.args.get("url")
 
     if not redirect_url:
         return "No URL provided", 400
-    
-    if safe (redirect_url):
+
+    if safe(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
-#requests used to prevent all automatic redirects
-#only allows URLs that respond without redirects
+
+# requests used to prevent all automatic redirects
+# only allows URLs that respond without redirects
+
 
 def safe(url):
     redirect = requests.get(url, allow_redirects=False)
     return redirect.status_code not in (301, 302, 307, 308)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_whitelist_redirects/test_case01.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_whitelist_redirects/test_case01.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = request.args.get("url")
@@ -11,37 +12,41 @@ def unvalidated_redirect():
         return "No URL provided", 400
     return redirect(redirect_url)
 
+
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = request.args.get("url")
 
     if not redirect_url:
         return "No URL provided", 400
-    
-    if safe (redirect_url):
+
+    if safe(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
-#urllib used to check for allowable redirect links
-#checks for an entire link and whether or not it is secure
+
+# urllib used to check for allowable redirect links
+# checks for an entire link and whether or not it is secure
+
 
 def safe(url):
-    parsed_url=urlparse(url)
+    parsed_url = urlparse(url)
 
-    domains = {"www.allowed.com","www.safe.com", "www.secure.com"}
-    paths = {"/safe", "/allowed", "/secure", "", "/"} 
+    domains = {"www.allowed.com", "www.safe.com", "www.secure.com"}
+    paths = {"/safe", "/allowed", "/secure", "", "/"}
     ## "" and "/" cases --> if no path is entered
 
-    return (    
-        (parsed_url.scheme == "http" or parsed_url.scheme == "https") and
-        parsed_url.netloc in domains and 
-        parsed_url.path in paths
+    return (
+        (parsed_url.scheme == "http" or parsed_url.scheme == "https")
+        and parsed_url.netloc in domains
+        and parsed_url.path in paths
     )
 
-    #checks scheme for http or https
-    #checks domain for allowable domains
-    #checks paths for allowable paths within those domains
+    # checks scheme for http or https
+    # checks domain for allowable domains
+    # checks paths for allowable paths within those domains
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
