2dce0c42e2a7e087ed7fa854d141010ed70dfdb3
Benson Liu <bensonhliu@gmail.com>
2025-07-14 00:18:51 -0400

    fix(style): ran formatter

--- plugins/cwe_352_csrf/__init__.py
+++ plugins/cwe_352_csrf/__init__.py
@@ -19,15 +19,18 @@ SINKS = []
 import dataset.cwe_352_csrf.flask_wtf_post.app
 from dataset.cwe_352_csrf.flask_wtf_post.app import csrf
 
+
 # # sanitizer
 def is_csrf_vulnerable(app: Flask, endpoint: str):
     view_func = app.view_functions.get(endpoint)
     full_name = f"{view_func.__module__}.{view_func.__name__}"
     is_csrf_exempt = full_name in csrf._exempt_views
-    return "flask_wtf.csrf" not in sys.modules or is_csrf_exempt 
+    return "flask_wtf.csrf" not in sys.modules or is_csrf_exempt
 
 
 old_insecure_update = dataset.cwe_352_csrf.flask_wtf_post.app.insecure_update
+
+
 @csrf.exempt
 @dataset.cwe_352_csrf.flask_wtf_post.app.login_required
 def new_insecure_update():
@@ -37,8 +40,11 @@ def new_insecure_update():
     if is_csrf_vulnerable(app, endpoint):
         raise TaintException("possible vulnerability")
     return old_insecure_update()
-dataset.cwe_352_csrf.flask_wtf_post.app.app.view_functions['insecure_update'] = new_insecure_update
 
+
+dataset.cwe_352_csrf.flask_wtf_post.app.app.view_functions["insecure_update"] = (
+    new_insecure_update
+)
 
 
 @pytest.fixture()
@@ -49,13 +55,16 @@ def app():
 
     yield app
 
+
 @pytest.fixture()
 def client(app):
     return app.test_client(use_cookies=True)
 
+
 @pytest.fixture()
 def fuzzer(app):
     return DictionaryFuzzer(app, "plugins/cwe_352_csrf/dictionary.txt")
+
 
 def test_fuzz(app, fuzzer):
     from urllib.parse import urlencode
@@ -68,15 +77,20 @@ def test_fuzz(app, fuzzer):
             response = victim.post("/register?username=test&password=test")
             response = victim.post("/login?username=test&password=test")
 
-            session_cookie = victim.get_cookie("session") 
+            session_cookie = victim.get_cookie("session")
             assert session_cookie is not None
 
             with pytest.raises(TaintException):
-                attacker.set_cookie(domain="localhost", key="session", value=session_cookie.value)
+                attacker.set_cookie(
+                    domain="localhost", key="session", value=session_cookie.value
+                )
                 # some test files use get instead
-                response = attacker.post(f"/insecure-update?new_password={urlencode({'file': data})}")
+                response = attacker.post(
+                    f"/insecure-update?new_password={urlencode({'file': data})}"
+                )
                 print(response.data.decode())
             counter += 1
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main([__file__]))
