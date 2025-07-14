d409b2aab0b52e70f726edd44ff4846d05364533
unknown <carterkylechew@gmail.com>
2025-07-14 15:11:55 -0400

    Formatted test cases using ruff

--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_prevent_hostname_tricks/test_case03.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_prevent_hostname_tricks/test_case03.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = request.args.get("url")
@@ -10,31 +11,35 @@ def unvalidated_redirect():
         return "No URL provided", 400
     return redirect(redirect_url)
 
+
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = request.args.get("url")
 
     if not redirect_url:
         return "No URL provided", 400
-    
-    if safe (redirect_url):
+
+    if safe(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
-#urllib used to prevent open redirects via hostname tricks
+
+# urllib used to prevent open redirects via hostname tricks
+
 
 def safe(url):
-    parsed_url=urlparse(url)
+    parsed_url = urlparse(url)
 
-    domains = {"www.allowed.com","www.safe.com", "www.secure.com"}
+    domains = {"www.allowed.com", "www.safe.com", "www.secure.com"}
     hostname = parsed_url.hostname
-    hostname = hostname.lower().strip('.')
+    hostname = hostname.lower().strip(".")
 
-    #hostname tricks like url=https://safe.com@unsafe.org won't work
-    #hostname.lower().strip('.') returns only the hostname, unsafe.org ^
+    # hostname tricks like url=https://safe.com@unsafe.org won't work
+    # hostname.lower().strip('.') returns only the hostname, unsafe.org ^
 
     return hostname in domains
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_whitelist_paths/test_case02.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_whitelist_paths/test_case02.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = request.args.get("path")
@@ -10,31 +11,35 @@ def unvalidated_redirect():
         return "No URL provided", 400
     return redirect(redirect_url)
 
+
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = request.args.get("path")
 
     if not redirect_url:
         return "No URL provided", 400
-    
-    if safe (redirect_url):
+
+    if safe(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
-#urllib used to check for allowable relative-url paths
-#doesn't allow redirect to other domains; only relative, safe, paths
+
+# urllib used to check for allowable relative-url paths
+# doesn't allow redirect to other domains; only relative, safe, paths
+
 
 def safe(path):
-    parsed_url=urlparse(path)
+    parsed_url = urlparse(path)
 
     paths = {"/safe", "/allowed", "/secure"}
 
     return (
-        (parsed_url.scheme == "" or parsed_url.scheme == None) and
-        (parsed_url.netloc == "" or parsed_url.netloc == None) and
-        parsed_url.path in paths
+        (parsed_url.scheme == "" or parsed_url.scheme == None)
+        and (parsed_url.netloc == "" or parsed_url.netloc == None)
+        and parsed_url.path in paths
     )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
