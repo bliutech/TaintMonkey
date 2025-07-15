4d9fa62697c61971fdc17f675959f03e117484f0
unknown <carterkylechew@gmail.com>
2025-07-14 21:26:32 -0400

    Rewrote test cases 01-06 to create secure sanitizers

--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_deny_list_urls/test_case03.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_deny_list_urls/test_case03.py
@@ -3,10 +3,12 @@ from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+DENY_LIST = {"www.malicious.com", "www.evil.com", "www.unsafe.com"}
+
 
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
-    redirect_url = request.args.get("path")
+    redirect_url = request.args.get("url")
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
@@ -14,7 +16,7 @@ def unvalidated_redirect():
 
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
-    redirect_url = request.args.get("path")
+    redirect_url = request.args.get("url")
 
     if not redirect_url:
         return "No URL provided", 400
@@ -25,20 +27,21 @@ def validated_redirect():
     return "Invalid redirect URL", 400
 
 
-# urllib used to check for allowable relative-url paths
-# doesn't allow redirect to other domains; only relative, safe, paths
+# urllib used to check for denyable redirect links
+# checks for an entire link and whether or not it is secure
 
 
-def safe(path):
-    parsed_url = urlparse(path)
-
-    paths = {"/safe", "/allowed", "/secure"}
+def safe(url):
+    parsed_url = urlparse(url)
 
     return (
-        (parsed_url.scheme == "" or parsed_url.scheme == None)
-        and (parsed_url.netloc == "" or parsed_url.netloc == None)
-        and parsed_url.path in paths
+        (parsed_url.scheme == "http" or parsed_url.scheme == "https")
+        and parsed_url.netloc not in DENY_LIST
     )
+
+    # checks scheme for http or https
+    # checks domain for denyable domains
+    # checks paths for denyable paths within those domains
 
 
 if __name__ == "__main__":
