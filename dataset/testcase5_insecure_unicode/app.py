776a57fd19e55bbe0f82cebebb7ebae17c46c280
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-07-15 19:08:30 -0400

    feat(data): test case 6, unicode prevention, created test case 5 but may delete

---
+++ dataset/testcase5_insecure_unicode/app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+def open_file_command(file):
+    return "cat {}".format(file)
+
+@app.get("/insecure")
+def insecure_handler():
+    file = request.args.get("file") or "example.txt"
+    result = os.popen(open_file_command(file)).read()
+
+    return result
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
