7cabafa5ffba31db6edd195f2b2bac3fc2f2b4ef
unknown <carterkylechew@gmail.com>
2025-07-15 22:03:27 -0400

    Reformatted with Ruff

--- dataset/cwe_938_unvalidated_redirects_and_forwards/furl_allow_list_urls/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/furl_allow_list_urls/app.py
@@ -5,14 +5,12 @@ app = Flask(__name__)
 
 ALLOW_LIST = {"www.allowed.com", "www.safe.com", "www.secure.com"}
 ALLOW_PATHS = {"/safe", "/allowed", "/secure", "", "/"}
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = get_url()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = get_url()
@@ -24,10 +22,8 @@ def validated_redirect():
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
-
 def get_url():
     return request.args.get("url")
-
 def check_allow_list(url):
 # furl used to check for allowable urls
     parsed_url = furl(url)
@@ -37,6 +33,5 @@ def check_allow_list(url):
         and parsed_url.host in ALLOW_LIST
         and str(parsed_url.path) in ALLOW_PATHS
     )
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_allow_list_relative_paths/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_allow_list_relative_paths/app.py
@@ -4,14 +4,12 @@ from yarl import URL
 app = Flask(__name__)
 
 ALLOW_RELATIVE_PATHS = {"/safe", "/allowed", "/secure"}
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = get_path()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = get_path()
@@ -22,10 +20,8 @@ def validated_redirect():
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
-
 def get_path():
     return request.args.get("path")
-
 def check_allow_path(path):
 # yarl used to check for allowable relative paths
     parsed_url = URL(path)
@@ -35,6 +31,5 @@ def check_allow_path(path):
         and (parsed_url.host == "" or parsed_url.host == None)
         and parsed_url.path in ALLOW_RELATIVE_PATHS
     )
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
