e22d180095c7286b47a0f0417bdbebe2c4610488
unknown <carterkylechew@gmail.com>
2025-07-15 22:09:01 -0400

    chore(format) Edited test cases 01-10

--- dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_allow_list_urls/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_allow_list_urls/app.py
@@ -5,12 +5,16 @@ app = Flask(__name__)
 
 ALLOW_LIST = {"www.allowed.com", "www.safe.com", "www.secure.com"}
 ALLOW_PATHS = {"/safe", "/allowed", "/secure", "", "/"}
+
+
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = get_url()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
+
+
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = get_url()
@@ -22,10 +26,14 @@ def validated_redirect():
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
+
+
 def get_url():
     return request.args.get("url")
+
+
 def check_allow_list(url):
-# yarl used to check for allowable urls
+    # yarl used to check for allowable urls
     parsed_url = URL(url)
 
     return (
@@ -33,5 +41,7 @@ def check_allow_list(url):
         and parsed_url.host in ALLOW_LIST
         and parsed_url.path in ALLOW_PATHS
     )
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
