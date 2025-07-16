4bf9116edabd22dcbb46c64623a1ed46e7d8d092
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-07-16 01:39:49 -0400

    fix(data): fixed __init__.py to match new folders

--- plugins/cwe_78_os_command_injection/__init__.py
+++ plugins/cwe_78_os_command_injection/__init__.py
@@ -44,7 +44,7 @@ def new_popen(cmd: TaintedStr, mode: str = "r", buffering: int = -1) -> os._wrap
 
 
 # Patch utility functions
-import dataset.cwe_78_testcases.testcase1_novalidation.app
+import dataset.cwe_78_testcases.testcase12_novalidation.app
 
 old_open_file_command = (
     dataset.cwe_78_testcases.testcase1_novalidation.app.open_file_command
@@ -52,7 +52,7 @@ old_open_file_command = (
 
 
 @patch_function(
-    "dataset.cwe_78_testcases.testcase1_novalidation.app.open_file_command"
+    "dataset.cwe_78_testcases.testcase12_novalidation.app.open_file_command"
 )
 def new_open_file_command(file: TaintedStr):
     return TaintedStr(old_open_file_command(file))
@@ -61,7 +61,7 @@ def new_open_file_command(file: TaintedStr):
 old_is_safe_path = dataset.cwe_78_testcases.testcase1_novalidation.app.is_safe_path
 
 
-@patch_function("dataset.cwe_78_testcases.testcase1_novalidation.app.is_safe_path")
+@patch_function("dataset.cwe_78_testcases.testcase12_novalidation.app.is_safe_path")
 def new_is_safe_path(path: TaintedStr):
     path.sanitize()
     return old_is_safe_path(path)
@@ -70,7 +70,7 @@ def new_is_safe_path(path: TaintedStr):
 # https://flask.palletsprojects.com/en/stable/testing/
 @pytest.fixture()
 def app():
-    from dataset.cwe_78_testcases.testcase1_novalidation.app import app
+    from dataset.cwe_78_testcases.testcase12_novalidation.app import app
 
     register_taint_client(app)
 
