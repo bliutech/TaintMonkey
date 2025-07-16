3e6de5aa26132a6f762101f920d63482fd24eb8c
unknown <carterkylechew@gmail.com>
2025-07-15 22:03:27 -0400

    Reformatted with Ruff

--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_allow_list_relative_paths/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_allow_list_relative_paths/app.py
@@ -4,14 +4,12 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 ALLOW_RELATIVE_PATHS = {"/safe", "/allowed", "/secure"}
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = get_path()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = get_path()
@@ -22,10 +20,8 @@ def validated_redirect():
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
-
 def get_path():
     return request.args.get("path")
-
 def check_allow_path(path):
 # urllib used to check for allowable relative-url paths
     parsed_url = urlparse(path)
@@ -35,6 +31,5 @@ def check_allow_path(path):
         and (parsed_url.netloc == "" or parsed_url.netloc == None)
         and parsed_url.path in ALLOW_RELATIVE_PATHS
     )
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_deny_list_urls/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_deny_list_urls/app.py
@@ -4,14 +4,12 @@ from urllib.parse import urlparse
 app = Flask(__name__)
 
 DENY_LIST = {"www.malicious.com", "www.evil.com", "www.unsafe.com"}
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
     redirect_url = get_url()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
     redirect_url = get_url()
@@ -23,10 +21,8 @@ def validated_redirect():
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
-
 def get_url():
     return request.args.get("url")
-
 def check_deny_list(url):
 # urllib used to check for denyable redirect links
     parsed_url = urlparse(url)
@@ -35,6 +31,5 @@ def check_deny_list(url):
         parsed_url.scheme == "http" or parsed_url.scheme == "https"
         and parsed_url.netloc not in DENY_LIST
     )
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
