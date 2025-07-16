6d6a758f9fcf2f01a45bdaa6db0cece8219e6fa3
unknown <carterkylechew@gmail.com>
2025-07-15 21:37:02 -0400

    Renamed functions; fixed comments

--- dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_allow_list_urls/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/urllib_allow_list_urls/app.py
@@ -1,39 +1,35 @@
 from flask import Flask, request, redirect
 from urllib.parse import urlparse
 
-
 app = Flask(__name__)
 
 ALLOW_LIST = {"www.allowed.com", "www.safe.com", "www.secure.com"}
 ALLOW_PATHS = {"/safe", "/allowed", "/secure", "", "/"}
 
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
-    redirect_url = request.args.get("url")
+    redirect_url = get_url()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
 
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
-    redirect_url = request.args.get("url")
+    redirect_url = get_url()
 
     if not redirect_url:
         return "No URL provided", 400
 
-    if safe(redirect_url):
+    if check_allow_list(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
+def get_url():
+    return request.args.get("url")
 
+def check_allow_list(url):
 # urllib used to check for allowable redirect links
-# checks for an entire link and whether or not it is secure
-
-
-def safe(url):
     parsed_url = urlparse(url)
 
     return (
@@ -41,11 +37,6 @@ def safe(url):
         and parsed_url.netloc in ALLOW_LIST
         and parsed_url.path in ALLOW_PATHS
     )
-
-    # checks scheme for http or https
-    # checks domain for allowable domains
-    # checks paths for allowable paths within those domains
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
--- dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_deny_list_urls/app.py
+++ dataset/cwe_938_unvalidated_redirects_and_forwards/yarl_deny_list_urls/app.py
@@ -5,38 +5,36 @@ app = Flask(__name__)
 
 DENY_LIST = {"www.malicious.com", "www.evil.com", "www.unsafe.com"}
 
-
 @app.route("/unvalidated_redirect", methods=["GET"])
 def unvalidated_redirect():
-    redirect_url = request.args.get("url")
+    redirect_url = get_url()
     if not redirect_url:
         return "No URL provided", 400
     return redirect(redirect_url)
 
-
 @app.route("/validated_redirect", methods=["GET"])
 def validated_redirect():
-    redirect_url = request.args.get("url")
+    redirect_url = get_url()
 
     if not redirect_url:
         return "No URL provided", 400
 
-    if safe(redirect_url):
+    if check_deny_list(redirect_url):
         return redirect(redirect_url)
 
     return "Invalid redirect URL", 400
 
+def get_url():
+    return request.args.get("url")
 
+def check_deny_list(url):
 # yarl used to check for denyable domains
-
-
-def safe(url):
     parsed_url = URL(url)
 
     return (
         parsed_url.scheme == "http" or parsed_url.scheme == "https"
-    ) and parsed_url.host not in DENY_LIST
-
+        and parsed_url.host not in DENY_LIST
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
