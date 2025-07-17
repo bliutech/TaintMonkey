aaea7f6d61d24f8a5680bb80c2fd92b6dbf8f325
unknown <carterkylechew@gmail.com>
2025-07-17 15:26:51 -0400

    Edited test case 01 to change sanitizer to allow list

--- dataset/cwe_918_server_side_request_forgery/test_case01.py
+++ dataset/cwe_918_server_side_request_forgery/test_case01.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
-import requests, socket, ipaddress
+import requests
 from urllib.parse import urlparse
 
 app = Flask(__name__)
+
+ALLOW_LIST = {"www.allowed.com", "www.safe.com", "www.secure.com"}
+ALLOW_PATHS = {"/safe", "/allowed", "/secure", "", "/"}
 
 @app.route("/insecure")
 def insecure_route():
@@ -18,23 +21,20 @@ def secure_route():
     if not url:
         return "Invalid url input", 400
 
-    if url_is_public(url):
+    if url_is_allowed(url):
         return requests.get(url).text
     
-    return "Url is not public"
+    return "Url is not allows"
 
-def url_is_public(url):
-    try:
-        parsed_url_host = urlparse(url).netloc
-        if not parsed_url_host:
-            return False
-        ip = socket.gethostbyname(parsed_url_host)
-        ip = ipaddress.ip_address(ip)
+def url_is_allowed(url):
 
-        return ip.is_global
-    
-    except (ValueError, socket.gaierror):
-        return False
+    parsed_url = urlparse(url)
+
+    return (
+        (parsed_url.scheme == "http" or parsed_url.scheme == "https")
+        and parsed_url.netloc in ALLOW_LIST
+        and parsed_url.path in ALLOW_PATHS
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
