701b8ef3c1b814081f5bd40d349529c53878442f
Aarav Parikh <aaravp1223@gmail.com>
2025-07-18 11:10:20 +0000

    feat(add): reformat

--- dataset/cwe_434_unrestricted_upload_file/testcase2/app.py
+++ dataset/cwe_434_unrestricted_upload_file/testcase2/app.py
@@ -6,36 +6,39 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
 @app.post("/insecure_upload")
 def insecure_upload():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "No file part", 400
 
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         return "No selected file", 400
 
     path = os.path.join(UPLOAD_FOLDER, file.filename)
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         f.write(file.read())
 
     return f"File {file.filename} uploaded insecurely"
 
+
 @app.post("/secure_upload")
 def secure_upload():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return "No file part", 400
 
-    file = request.files['file']
-    if file.filename == '':
+    file = request.files["file"]
+    if file.filename == "":
         return "No selected file", 400
-    
-    filename = secure_filename(file.filename) # sanitizer
+
+    filename = secure_filename(file.filename)  # sanitizer
     path = os.path.join(UPLOAD_FOLDER, filename)
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         f.write(file.read())
 
     return f"File {filename} uploaded securely."
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
