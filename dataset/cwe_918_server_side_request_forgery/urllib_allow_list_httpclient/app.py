9564dd85980feccf8984970abb1683c1783910b7
unknown <carterkylechew@gmail.com>
2025-07-18 02:31:04 -0400

    Re-added app.py

---
+++ dataset/cwe_918_server_side_request_forgery/urllib_allow_list_httpclient/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+import http.client
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+
+ALLOW_LIST = {"www.allowed.com", "www.safe.com", "www.secure.com"}
+ALLOW_PATHS = {"/safe", "/allowed", "/secure", "", "/"}
+
+
+@app.route("/insecure")
+def insecure_route():
+    url = get_url()
+    if not url:
+        return "Invalid url input", 400
+    return http_request(url)
+
+
+@app.route("/secure")
+def secure_route():
+    url = get_url()
+    if not url:
+        return "Invalid url input", 400
+    if check_allow_list(url):
+        return http_request(url)
+    return "Url is not allowed"
+
+
+def check_allow_list(url):
+    # urllib used to check for allowable links
+    parsed_url = urlparse(url)
+    return (
+        (parsed_url.scheme == "http" or parsed_url.scheme == "https")
+        and parsed_url.netloc in ALLOW_LIST
+        and parsed_url.path in ALLOW_PATHS
+    )
+
+
+def get_url():
+    return request.args.get("url")
+
+
+def http_request(url):
+    url = urlparse(url)
+    connection = http.client.HTTPConnection(url.netloc, url.port)
+    connection.request("GET", url.path)
+    return connection.getresponse()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
