88b0fe2c9e318fee03eaa02011a89e368dce39c9
Aarav Parikh <aaravp1223@gmail.com>
2025-07-18 18:18:00 +0000

    feat(add): edit app.py

--- dataset/cwe_434_unrestricted_upload_file/testcase6/app.py
+++ dataset/cwe_434_unrestricted_upload_file/testcase6/app.py
@@ -1,43 +1,89 @@
 import os
-from flask import Flask, request
-from werkzeug.utils import secure_filename
+from flask import (
+    Flask,
+    request,
+    redirect,
+    url_for,
+    send_from_directory,
+    render_template,
+)
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config["UPLOAD_FOLDER"] = "uploads/"
+
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 
-@app.post("/insecure_upload")
+@app.route("/insecure/upload", methods=["POST"])
 def insecure_upload():
     if "file" not in request.files:
         return "No file part", 400
 
     file = request.files["file"]
+
     if file.filename == "":
         return "No selected file", 400
 
-    path = os.path.join(UPLOAD_FOLDER, file.filename)
-    with open(path, "wb") as f:
-        f.write(file.read())
+    filename = file.filename
 
-    return f"File {file.filename} uploaded insecurely"
+    if (
+        filename.endswith(".jpg")
+        or filename.endswith(".png")
+        or filename.endswith(".gif")
+    ):
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return redirect(url_for("uploaded_file", filename=filename))
+    else:
+        return "File type not allowed.", 400
 
 
-@app.post("/secure_upload")
+@app.route("/secure/upload", methods=["POST"])
 def secure_upload():
     if "file" not in request.files:
         return "No file part", 400
 
     file = request.files["file"]
+
     if file.filename == "":
         return "No selected file", 400
 
-    filename = secure_filename(file.filename)  # sanitizer
-    path = os.path.join(UPLOAD_FOLDER, filename)
-    with open(path, "wb") as f:
-        f.write(file.read())
+    filename = file.filename
 
-    return f"File {filename} uploaded securely."
+    allowed_extensions = {"jpg", "jpeg", "png", "gif"}
+    if (
+        "." not in filename
+        or filename.rsplit(".", 1)[1].lower() not in allowed_extensions
+    ):
+        return "Only image files are allowed", 400
+
+    file_content = file.read(1024)
+    file.seek(0)
+
+    is_valid_image = False
+
+    # checks the "magic byte" to validate the image type
+    # jpeg
+    if file_content.startswith(b"\xff\xd8\xff"):
+        is_valid_image = True
+
+    # png
+    elif file_content.startswith(b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a"):
+        is_valid_image = True
+
+    # gif
+    elif file_content.startswith(b"GIF87a") or file_content.startswith(b"GIF89a"):
+        is_valid_image = True
+
+    if not is_valid_image:
+        return "File content is not an allowed image types", 400
+
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return redirect(url_for("uploaded_file", filename))
+
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 
 if __name__ == "__main__":
