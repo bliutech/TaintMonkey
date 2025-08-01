9398a29570449b631799fd8b2e2a484cf88b7e61
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2025-08-01 14:24:22 -0400

    (fix): insecure_login function for plugin

--- dataset/cwe_89_sql_injection/insecure_login/app.py
+++ dataset/cwe_89_sql_injection/insecure_login/app.py
@@ -5,6 +5,9 @@ from db import db, init_db
 app = Flask(__name__)
 init_db(app)
 
+def insecure_input(username):
+    query = text(f"SELECT * FROM user WHERE username = '{username}'")
+    return query
 
 @app.route("/insecure-second-level", methods=["GET"])
 def insecure_second_level():
@@ -14,7 +17,7 @@ def insecure_second_level():
         return "Username is required", 400
 
     # second-level, getting from database when needed level, takes literal string
-    query = text(f"SELECT * FROM user WHERE username = '{username}'")
+    query = insecure_input(username)
 
     try:
         result = db.session.execute(query).fetchall()
@@ -24,7 +27,6 @@ def insecure_second_level():
         return "No users found", 404
     except Exception as e:
         return f"Error: {str(e)}", 500
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
