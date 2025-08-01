86d6243fa9e5844b165feff8e6db7a2648d95495
Aarav Parikh <aaravp1223@gmail.com>
2025-08-01 02:06:44 +0000

    feat(add): seperate insecure and secure

--- dataset/cwe_79_cross_site_scripting/insecure_bleach_clean_response/app.py
+++ dataset/cwe_79_cross_site_scripting/insecure_bleach_clean_response/app.py
@@ -15,16 +15,5 @@ def insecure_xss():
     return welcome(name)
 
 
-@app.get("/secure")
-def secure_xss():
-    name = request.args.get("name", "user")
-    upd_name = bleach.clean(name)
-
-    if name != upd_name:
-        return "Invalid input detected and cleaned", 400
-
-    return welcome(name)
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
---
+++ dataset/cwe_79_cross_site_scripting/secure_bleach_clean_response/app.py
@@ -0,0 +1,24 @@
+# https://bleach.readthedocs.io/en/latest/clean.html
+from flask import Flask, request
+import bleach
+
+app = Flask(__name__)
+
+
+def welcome(name):
+    return f"<h1>Welcome to GSET, {name}!</h1>"
+
+
+@app.get("/secure")
+def secure_xss():
+    name = request.args.get("name", "user")
+    upd_name = bleach.clean(name)
+
+    if name != upd_name:
+        return "Invalid input detected and cleaned", 400
+
+    return welcome(name)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
